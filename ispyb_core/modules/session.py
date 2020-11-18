73953867fe25f5a044bcf2e2c14a69915b7c6518
Neil A Smith <neil.smith@diamond.ac.uk>
2020-11-18 09:17:20 +0000

    Example of query filters

--- ispyb_core/modules/session.py
+++ ispyb_core/modules/session.py
@@ -22,12 +22,14 @@ along with py-ispyb. If not, see <http://www.gnu.org/licenses/>.
 
 __license__ = "LGPLv3+"
 
+from sqlalchemy import func
 
-from app.extensions import db
+from app.extensions import db, auth_provider
 
 from ispyb_core import models, schemas
 
 from ispyb_core.modules import beamline_setup, data_collection, proposal
+from ispyb_core.modules import contacts
 
 
 def get_sessions(request):
@@ -38,15 +40,75 @@ def get_sessions(request):
 
     Returns:
         [type]: [description]
+
+
+    From SynchWeb valid query parameters include:
+    prop (injected as a parameter for all queries when a proposal is selected)
+    current (get the next, prev and cm visit for each beamline by type)
+    all (find sessions across proposals - else get a proposal id via prop. The normal case is we want sessions from a given proposal)
+    year
+    month
+    prev (last visit/session for this proposal)
+    next (next upcoming visit/session for this proposal)
+    started
+    bl (beamline)
+    cm (commissioning)
+    ty (specific proposal types, mx, em etc)
+    s (search term)
+    scheduled
+    per_page (number of records to return - equivalent to limit)
+    page     (the page number - equivalent to offset)
+    sort_by (can be ST = start date, EN = end date, VIS = visit_number, BL = beamline, LC = beamline operator, COMMENT = comment (not sure used that much!))
+    order (can be 'asc' = ascending, else descending)
+
+    We also append the data collection count, proposal type (based on beamline)
+
     """
+    user_info = auth_provider.get_user_info_by_auth_header(
+        request.headers.get("Authorization")
+    )
+
+    person_id = contacts.get_person_id_by_login(user_info["username"])
+
+    # Primary table is the sessions table
+    query = models.BLSession.query
+
+    # Marshall the query parameters
     query_params = request.args.to_dict()
 
-    return db.get_db_items(
-        models.BLSession,
-        schemas.session.dict_schema,
-        schemas.session.ma_schema,
-        query_params,
-    )
+    # Session zero is used for special cases, 'all' refers to actual user sessions
+    if 'all' in query_params:
+        query = query.filter(models.BLSession.visit_number > 0)
+    # Has a proposal been requested?
+    else if 'prop' in query_params:
+        query = query.join(models.Proposal).\
+                filter(models.Proposal.proposalId == models.BLSession.proposalId).\
+                filter(func.concat(models.Proposal.proposalCode,models.Proposal.proposalNumber) == query_params['prop'])
+
+    # Determine which sessions this user should be able to see (assuming they are not staff/admin)
+    if not user_info.get("is_admin") and person_id:
+        # In reality we would want to know what sessions are coming, past, commissioning
+        query = query.join(models.SessionHasPerson).\
+            filter(models.SessionHasPerson.sessionId == models.BLSession.sessionId).\
+            filter(models.SessionHasPerson.personId == person_id)
+
+    if 'bl' in query_params:
+        query = query.filter(models.BLSession.beamLineName == query_params['bl'])
+
+    if 'per_page' in query_params:
+        query = query.limit(query_params['per_page'])
+
+    if 'page' in query_params:
+        query = query.offset(query_params['page'])
+
+    data = schemas.session.ma_schema.dump(query, many=True)
+
+    response = {}
+    response['total'] = len(data[0])
+    response['data'] = data[0]
+
+    return response
+
 
 
 def add_session(data_dict):
